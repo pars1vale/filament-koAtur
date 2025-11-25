6822208e37405c3c05f39b825cad982274a1fa08
diimprasetyos <dimasid290503@gmail.com>
2025-11-25 16:17:52 +0700

    feat: change admin panel auth to breeze

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,6 @@ Route::post('/admin/logout', function () {
     return redirect('/login');
 })->name('filament.admin.auth.logout');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
